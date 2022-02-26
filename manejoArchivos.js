a69701cce55c00440b91131d910e35b73f98a7af
Buxtehud <andres.anreyes@gmail.com>
2022-02-26 17:27:34 -0300

    Correción de desafío

--- manejoArchivos.js
+++ manejoArchivos.js
@@ -2,61 +2,58 @@ const fs = require('fs')
 
 class Contenedor{
     constructor(fileName) {
-        this.fileName = fileName;
+        this.fileName = `./${fileName}`;
     }
 
-    save(obj){
-        const read = async (string, pathFile) => {
-            try {
-                const file = await fs.promises.readFile(pathFile, 'utf-8');
-                const fileObj = JSON.parse(file);
+    async readData(){
+        try{
+            const file = await fs.promises.readFile(this.fileName,'utf-8');
+            return JSON.parse(file);
+        } catch (err) {
+            return []
+        }
+    }
+
+    async save(obj){
+        this.readData().then((fileObj) => {
+            if (fileObj.length > 0) {
                 obj.id = fileObj[fileObj.length - 1].id + 1;
-                fileObj.push(obj);
-                const newObject = JSON.stringify(fileObj);
-                console.log(fileObj);
-                fs.writeFileSync(pathFile,newObject);
-            } catch (err) {
+            } else {
                 obj.id = 1;
-                const newObject = JSON.stringify([obj]);
-                fs.writeFileSync(pathFile, newObject);
             }
-        }
-        read(obj, this.fileName);
+            fileObj.push(obj);
+            const newObject = JSON.stringify(fileObj);
+            fs.promises.writeFile(this.fileName, newObject);
+        })
     }
 
-    getById(id){
-        const file = fs.readFileSync(`./${this.fileName}`, 'utf-8');
-        const fileObj = JSON.parse(file);
-        const element = fileObj.filter(element => element.id === id);
-        if (element.length > 0) {
-            return element;
-        } else {
-            return null
-        }
+    async getById(id){
+        return this.readData().then(fileObj => {
+            let objFound = fileObj.find(element => element.id === id);
+            return objFound ? objFound : null;
+        });
     }
 
-    getAll(){
-        const file = fs.readFileSync(`./${this.fileName}`, 'utf-8');
-        return JSON.parse(file);
+    async getAll(){
+        return this.readData().then(fileObj=> fileObj);
     }
 
-    deleteById(id){
-        const file = fs.readFileSync(`./${this.fileName}`, 'utf-8');
-        let fileObj = JSON.parse(file);
-        fileObj = fileObj.filter(element => element.id !== id);
-        const newObject = JSON.stringify(fileObj)
-        fs.writeFileSync(`./${this.fileName}`,newObject)
+    async deleteById(id){
+        this.readData().then(fileObj => {
+            fileObj = fileObj.filter(element => element.id !== id);
+            fs.promises.writeFile(this.fileName, JSON.stringify(fileObj));
+        })
     }
 
-    deleteAll(){
-        fs.writeFileSync(`./${this.fileName}`,'')
+    async deleteAll(){
+        await fs.promises.writeFile(this.fileName,'')
     }
 }
 
 const cont = new Contenedor('product.txt');
-//cont.save({title: 'JAbon', price:245, thumbnail: 'urlJabon'});
-console.log(cont.getById(2))
-console.log(cont.getById(7))
-cont.deleteById(3)
-console.log(cont.getAll())
-cont.deleteAll()
+cont.save({title: 'Sartén', price:685, thumbnail: 'urlSarten'}).then(() => console.log('Element added successfully'));
+cont.getById(2).then(ans => console.log(ans))
+cont.getById(7).then(ans => console.log(ans))
+cont.deleteById(3).then(() => console.log('Element deleted successfully'))
+cont.getAll().then(ans => console.log(ans))
+cont.deleteAll().then(() => console.log('File deleted successfully'))
